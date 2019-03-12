94621b8a3c6422c8cf1bc175bdfc1f342e76820c
NickHolden <niholden@umich.edu>
2019-03-12 13:20:52 -0400

    re

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "navRow") {
-    x.className += " responsive";
-  } else {
-    x.className = "navRow";
-  }
-}
--- week-3/README.md
+++
@@ -1,1 +0,0 @@
-# NickHolden.github.io
